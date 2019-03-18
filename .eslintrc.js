3459ea3358ee3d5c2e36b6088ceaebdd8a78a6f9
Eloi Simard-Quesnel <eloi.qs@outlook.com>
2019-03-17 22:30:17 -0400

    Fix eslint fix command on lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,9 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2018,
